1fcaec1e56d314d3b26e3ae23365ed3cded41c81
GrandHerestetic <93905625+GrandHerestetic@users.noreply.github.com>
2023-04-14 15:50:07 +0600

    final

---
+++ main.js
@@ -0,0 +1,13 @@
+function changeNavbar() {
+    document.getElementById("app").style.bottom = "30px";
+    document.getElementById("app").style.backgroundColor = "#88292f";
+    document.getElementById("app").style.border = "";
+  }
+
+  function rechangeNavbar() {
+    document.getElementById("app").style.bottom = "50px";
+    document.getElementById("app").style.backgroundColor = "#88292f";
+    document.getElementById("app").style.border = "1px solid black";
+    document.getElementById("link-1").style.color = "white";
+    document.getElementById("link-2").style.color = "white";
+  }
